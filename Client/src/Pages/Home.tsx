ead31d05f82adf7adbb5fabc26f61a4f8b9d1215
ASIA\Willy_Chang <d410413@gmail.com>
2025-08-22 08:52:21 +0800

    2025/08/22

---
+++ Client/src/Pages/Home.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <>
+      <div className="bg-red-500">123</div>
+    </>
+  );
+};
+export default Home;
---
+++ Client/src/Pages/Login/index.tsx
@@ -0,0 +1,7 @@
+export default () => {
+    return (
+        <>
+            <div>123</div>
+        </>
+    );
+};
